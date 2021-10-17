aca3e6c4a38ae75c3f26ad7cbe6ee5fa0ba9db37
baenaale <alejandrabaena32@gmail.com>
2021-10-17 12:29:19 -0500

    set default word and styled application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
         <header className="App-header">Dictionary</header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="Melon" />
         </main>
         <footer className="text-center">Coded by Alejandra Baena</footer>
       </div>
